98c499f915db61f6b7070195c007512756ed0380
qintalhq <itsmearhamkhan2012@gmail.com>
2026-07-29 15:29:32 +0530

    Change Supabase ANON_KEY to a new value
    Updated the Supabase anonymous key for security.

--- js/supabase.js
+++ js/supabase.js
@@ -4,7 +4,7 @@
 
 // Replace these with your own values
 const SUPABASE_URL = "https://purjntpgiycrhlqsesxz.supabase.co";
-const SUPABASE_ANON_KEY = "purjntpgiycrhlqsesxz";
+const SUPABASE_ANON_KEY = "sb_publishable_K1m-J1D2X1DnvO0h3O4DNw_VuPEq2_j";
 
 // Create Supabase client
 const supabaseClient = supabase.createClient(

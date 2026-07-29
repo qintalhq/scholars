3e7f7b7fa7d23f1a14e07b8537157ef1312c40a5
qintalhq <itsmearhamkhan2012@gmail.com>
2026-07-29 15:35:02 +0530

    Change Supabase anon key to a new value
    Updated Supabase anon key to a new value.

--- js/supabase.js
+++ js/supabase.js
@@ -4,7 +4,7 @@
 
 // Replace these with your own values
 const SUPABASE_URL = "https://purjntpgiycrhlqsesxz.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_K1m-J1D2X1DnvO0h3O4DNw_VuPEq2_j";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1cmpudHBnaXljcmhscXNlc3h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNTU3NDgsImV4cCI6MjEwMDczMTc0OH0.cnCgMp_nNXssYvC_-ftos32u4EsU1HAvwshi8MIsPgA";
 
 // Create Supabase client
 const supabaseClient = supabase.createClient(
